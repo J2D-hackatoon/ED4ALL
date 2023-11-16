4be527f792cbcf09aa7c7d932cb8f3d45a5d5500
treee97 <luduve97@gmail.com>
2023-11-16 22:25:02 +0100

    added types folder

--- src/components/SelectDistrict/SelectDistrict.tsx
+++ src/components/SelectDistrict/SelectDistrict.tsx
@@ -1,12 +1,18 @@
+import type { APIResults } from '../../types/types';
 import { useDistricts } from '../../utils/hooks/useDistricts';
 import Select from 'react-select';
 
 export const SelectDistrict = (): JSX.Element => {
   const { isLoading, isError, data } = useDistricts();
 
-  console.log('DATAAA', data);
+  if (isLoading) {
+    return <div className='loading loading-spinner'></div>;
+  }
+  if (isError) {
+    return <p>Error loading districts!</p>;
+  }
 
-  const options = data?.map((district: any) => ({
+  const options = data?.map((district: APIResults) => ({
     value: district.district_code,
     label: district.district_name,
   }));
