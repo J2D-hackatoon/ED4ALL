3fded7a0d137e12a7849034bcdb9deb487bf339f
treee97 <luduve97@gmail.com>
2023-11-17 00:25:11 +0100

    slider feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@
 // import Card from "./components/Card/Card";
 
 // import { Aside } from "./components/Aside/Aside";
+import { Slider } from './components/Slider/Slider';
 
 function App(): JSX.Element {
   return (
     <>
       {/* <Aside /> */}
+      <Slider />
       {/* <Card /> */}
     </>
   );
