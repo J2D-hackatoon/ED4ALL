a62b9b493b64aeb9e6e133b78cc3f12898344df2
Your Name <you@example.com>
2023-11-16 17:00:18 +0100

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react';
 
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
-function App() {
-
+function App(): JSX.Element {
   return (
     <>
-      hello
+      <Header />
+      <Footer />
     </>
   );
 }
