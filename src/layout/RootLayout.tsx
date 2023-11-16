efaf9e7853844c8f475892f3f92c5c3c5cd45467
REALcuba <angelskcuba@gmail.com>
2023-11-16 22:44:42 +0100

    mapas de distritos y card

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
-import { Aside } from "../components/Aside/Aside"
+// import { Aside } from "../components/Aside/Aside"
 
 export interface RootLayoutProps { }
 export const RootLayout: React.FC<RootLayoutProps> = () => {
@@ -10,7 +10,7 @@ export const RootLayout: React.FC<RootLayoutProps> = () => {
         <div className="max-h-screen">
             <Header />
             <>
-                <Aside />
+                {/* <Aside /> */}
                 <main className=" flex  min-w-screen">
                     <Outlet />
                 </main>
