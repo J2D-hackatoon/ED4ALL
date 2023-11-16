7d493e20bc4a42bf357a19cfc56d614f1900839a
Javi Extremera <javi.extremera92@gmail.com>
2023-11-16 17:07:02 +0100

    installed react select

---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,10 @@
+
+import { SelectDistrict } from "../SelectDistrict/SelectDistrict"
+export const Aside = () => {
+  return (
+    // el aside ha de estar dentro de un parent con el main
+      <aside className="min-h-screen">
+        <SelectDistrict />
+      </aside>
+  )
+}
