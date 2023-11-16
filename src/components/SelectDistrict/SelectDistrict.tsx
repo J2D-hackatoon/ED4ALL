8d79efe1f71288307c298bf8abe9fca9499d389d
Javi Extremera <javi.extremera92@gmail.com>
2023-11-16 17:53:27 +0100

    temporary useEffect

--- src/components/SelectDistrict/SelectDistrict.tsx
+++ src/components/SelectDistrict/SelectDistrict.tsx
@@ -1,13 +1,43 @@
 import Select from "react-select"
+import {useState, useEffect} from "react"
+
 export const SelectDistrict = () => {
-    const options = [
-        // Aquí iran los distritos
-        { value: 'chocolate', label: 'distrito1' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-    ]
+
+  const [districts, setDistricts] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from your backend
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/districts");
+        const data = await response.json();
+
+        // Transform data into the format expected by react-select
+        const options = data.map((district: any) => ({
+          value: district.district_code,
+          label: district.district_name,
+        }));
+
+        // Set the options in the state
+        setDistricts(options);
+      } catch (error) {
+        console.error("Error fetching districts:", error);
+      }
+    };
+
+    // Call the fetchData function
+    fetchData();
+  }, []);
+
+
+    // const options = [
+    //     // Aquí iran los distritos
+    //     { value: 'chocolate', label: 'distrito1' },
+    //     { value: 'strawberry', label: 'Strawberry' },
+    //     { value: 'vanilla', label: 'Vanilla' }
+    // ]
 
   return (
-    <Select options={options} placeholder="Escoge un distrito"  className="w-1/4 rounded-md border border-accent min-w-[250px] md:min-w-[450px] outline-none cursor-pointer"/>
+    <Select options={districts} placeholder="Escoge un distrito"  className="w-1/4 rounded-md border border-accent min-w-[250px] md:min-w-[450px] outline-none cursor-pointer"/>
   )
 }
